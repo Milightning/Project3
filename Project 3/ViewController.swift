45eb89346c9da6cd0b55595ccff6c74aeba376aa
Kate Chirkina <milightning.ua@gmail.com>
2019-03-04 17:03:07 +0200

    Initial Commit

---
+++ Project 3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 3
+//
+//  Created by Catherine on 3/4/19.
+//  Copyright © 2019 Catherine Priazhnikova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
