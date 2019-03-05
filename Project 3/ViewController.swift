224ace3cd884d895b1ae60a1f792e15170604439
Kate Chirkina <milightning.ua@gmail.com>
2019-03-05 17:49:09 +0200

    Add table view controller and detail view controller

--- Project 3/ViewController.swift
+++ Project 3/ViewController.swift
@@ -8,12 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var pictures = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let fileManager = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fileManager.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasSuffix("png") {
+                pictures.append(item)
+            }
+        }
+        
     }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Country", for: <#T##IndexPath#>)
+    }
+
+    
+    
 
 
 }
